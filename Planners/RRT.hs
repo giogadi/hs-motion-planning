41a5c0d91557dcbcd3c73f86f8d8e5ec1b0d0c82
Luis G. Torres <lgtorres42@gmail.com>
2013-11-15 11:22:57 -0500

    Fixed big space leak in RRT!

--- Planners/RRT.hs
+++ Planners/RRT.hs
@@ -13,11 +13,11 @@ module Planners.RRT
 import Data.StateSpace
 import Data.MotionPlanningProblem
 
+import Data.List (minimumBy)
 import Debug.Trace (trace)
 import Data.Maybe (isJust, fromJust)
 import System.Random (RandomGen, mkStdGen, StdGen)
 import qualified Control.Monad.Random as CMR
-import qualified Data.Sequence as Seq
 import Data.List (foldl1', intercalate)
 import Data.Foldable (foldr', sum, toList)
 import Data.Function (on)
@@ -25,41 +25,17 @@ import Data.Function (on)
 -- import Test.Framework (testGroup)
 -- import Test.Framework.Providers.QuickCheck2 (testProperty)
 
-data RoseTree a = Node a (Seq.Seq (RoseTree a))
+data Node s = Root s | Node s (Node s)
 
-rootLabel :: RoseTree a -> a
-rootLabel (Node s _) = s
-
-treeSize :: RoseTree a -> Int
-treeSize (Node _ ts) = 1 + (Data.Foldable.sum $ fmap treeSize ts)
-
-treeEdges :: RoseTree a -> [(a,a)]
-treeEdges t = go t
-    where go (Node s ts) = let listT = toList ts
-                           in  (zip (repeat s) $ rootLabel `fmap` listT) ++ (concat (go `fmap` listT))
-
-type TreePath = [Int]
-
-getNodeAt :: RoseTree a -> TreePath -> a
-getNodeAt (Node l ts) [] = l
-getNodeAt (Node l ts) (p:ps) = getNodeAt (ts `Seq.index` p) ps
-
-getPathNodes :: RoseTree a -> TreePath -> [a]
-getPathNodes (Node l ts) [] = [l]
-getPathNodes (Node l ts) (p:ps) = l : (getPathNodes (ts `Seq.index` p) ps)
-
-addChildAt :: RoseTree a -> TreePath -> a -> (RoseTree a, Int)
-addChildAt (Node l ts) [] s = ((Node l (ts Seq.|> (Node s Seq.empty))), Seq.length ts)
-addChildAt (Node l ts) (p:ps) s = let (newChild,idx) = addChildAt (ts `Seq.index` p) ps s
-                                      newTree = Node l $ Seq.update p newChild ts
-                                  in  (newTree,idx)
+nodeState :: Node s -> s
+nodeState (Root s) = s
+nodeState (Node s _) = s
 
 data RRT s g = RRT
     { _problem  :: MotionPlanningProblem s g
     , _stepSize :: Double
-    , _tree     :: RoseTree s
-    , _stateIdx :: [(s, TreePath)]
-    , _solution :: Maybe TreePath}
+    , _nodes    :: [Node s]
+    , _solution :: Maybe (Node s)}
 
 getSpace :: RRT s g -> StateSpace s g
 getSpace = _stateSpace . _problem
@@ -77,50 +53,45 @@ getInterp :: RRT s g -> (s -> s -> Double -> s)
 getInterp rrt = _interpolate $ getSpace rrt
 
 getNumStates :: RRT s g -> Int
-getNumStates = treeSize . _tree
+getNumStates = length . _nodes
 
 writeRRT :: Show s => RRT s g -> String -> IO ()
 writeRRT rrt fileName = writeFile fileName $ intercalate "\n" edgeStrings
-    where edgeStrings = map stringFromEdge $ treeEdges (_tree rrt)
-          stringFromEdge (s1,s2) = (show s1) ++ " " ++ (show s2)
+    where edgeStrings = map stringFromEdge $ _nodes rrt
+          stringFromEdge (Root _) = ""
+          stringFromEdge (Node s p) = (show s) ++ " " ++ (show $ nodeState p)
 
 -- A strict implementation of minimumBy
-minimumBy' :: (a -> a -> Ordering) -> [a] -> a
-minimumBy' cmp = foldl1' min'
-    where min' x y = case cmp x y of
-                       GT -> y
-                       _  -> x
+-- minimumBy' :: (a -> a -> Ordering) -> [a] -> a
+-- minimumBy' cmp = foldl1' min'
+--     where min' x y = case cmp x y of
+--                        GT -> y
+--                        _  -> x
 
-nearestNode :: RRT s g -> s -> (s, TreePath)
-nearestNode rrt sample = let compareFn = compare `on` ((getNonMetricDist rrt $ sample) . fst)
-                         in  minimumBy' compareFn (_stateIdx rrt)
+nearestNode :: RRT s g -> s -> Node s
+nearestNode rrt sample = let compareFn = compare `on` ((getNonMetricDist rrt $ sample) . nodeState)
+                         in  minimumBy compareFn (_nodes rrt)
 
 extendRRT :: RRT s g -> s -> RRT s g
 extendRRT rrt sample =
-    let (near,ps) = nearestNode rrt sample
-        newState = let d = (getDist rrt) near sample
+    let near = nearestNode rrt sample
+        nearState = nodeState near
+        newState = let d = (getDist rrt) nearState sample
                    in  if d <= _stepSize rrt
-                       then (getInterp rrt) near sample 1.0
-                       else (getInterp rrt) near sample $ ((_stepSize rrt) / d)
-    in  if (getValidityFn rrt) near newState
-        then let (newTree, newIdx) = addChildAt (_tree rrt) ps newState
-                 newPath = ps ++ [newIdx]
+                       then nearState
+                       else (getInterp rrt) nearState sample $ ((_stepSize rrt) / d)
+    in  if (getValidityFn rrt) nearState newState
+        then let newNode = Node newState near
                  solution = if (_goalSatisfied $ _problem rrt) newState
-                            then Just newPath
+                            then Just newNode
                             else Nothing
-                 newRRT = RRT
-                          (_problem rrt)
-                          (_stepSize rrt)
-                          newTree
-                          ((newState,newPath) : (_stateIdx rrt))
-                          solution
-             in  newRRT
+             in  RRT (_problem rrt) (_stepSize rrt) (newNode : (_nodes rrt)) solution
         else rrt
 
 buildRRT :: RandomGen g => MotionPlanningProblem s g -> Double -> Int -> CMR.Rand g (RRT s g)
 buildRRT problem stepSize numIterations =
     let start = _startState problem
-        beginRRT = RRT problem stepSize (Node start Seq.empty) [(start, [])] Nothing
+        beginRRT = RRT problem stepSize [(Root start)] Nothing
     in  go beginRRT 0
     where
       go rrt iteration
@@ -135,7 +106,9 @@ getPathToGoal :: RandomGen g => RRT s g -> [s]
 getPathToGoal rrt =
   case _solution rrt of
     Nothing -> []
-    Just path -> getPathNodes (_tree rrt) path
+    Just goalNode -> reverse $ go goalNode
+  where go (Root s) = [s]
+        go (Node s p) = s : (go p)
 
 solveRRT :: RandomGen g => MotionPlanningProblem s g -> Double -> Int -> CMR.Rand g [s]
 solveRRT problem stepSize numIterations =
