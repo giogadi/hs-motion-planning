10301df366fcec9bcf70061284f5a9bd8e31acbe
Luis G. Torres <lgtorres42@gmail.com>
2014-10-07 15:41:34 -0400

    Added better testing for DynamicKdTree

--- app-src/Tests/KdTreeTest.hs
+++ app-src/Tests/KdTreeTest.hs
@@ -1,9 +1,10 @@
-import Data.Trees.KdTree
+import Data.Trees.KdTree as KDT
+import Data.Trees.DynamicKdTree as DKDT
 
 import Control.Monad
 import System.Exit
 
 main :: IO ()
 main = do
-  success <- runTests
+  success <- liftM2 (&&) KDT.runTests DKDT.runTests
   when (not success) exitFailure
